75a32195b91049227521be1c39a2863b9a607388
João Paulo Akira Sigue <marcostasato2005@gmail.com>
2025-12-08 23:58:11 -0300

    fix: cors

--- backend/src/main/java/com/marcos/backend/config/Cors.java
+++ backend/src/main/java/com/marcos/backend/config/Cors.java
@@ -9,7 +9,7 @@ public class Cors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500")
+                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization", "X-Custom-Header")
                 .allowCredentials(true);
